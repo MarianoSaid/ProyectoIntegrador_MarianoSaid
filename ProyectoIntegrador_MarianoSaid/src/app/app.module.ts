2ea8f7776d56ad18ecbd52c6171d18b1260f05eb
MarianoSaid <marianoj_said@hotmail.com>
2022-04-10 19:56:12 -0300

    Incorporación de banner al header

--- ProyectoIntegrador_MarianoSaid/src/app/app.module.ts
+++ ProyectoIntegrador_MarianoSaid/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoAPComponent,
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
