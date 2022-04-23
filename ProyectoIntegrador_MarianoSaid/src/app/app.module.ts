262e9bbae24c04a7f4d4d2f0ea6ed63cafa02244
MarianoSaid <marianoj_said@hotmail.com>
2022-04-22 22:25:46 -0300

    Creación de Habilidades (HyS)

--- ProyectoIntegrador_MarianoSaid/src/app/app.module.ts
+++ ProyectoIntegrador_MarianoSaid/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
+
 
 @NgModule({
   declarations: [
@@ -20,12 +22,14 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+     ],
   providers: [],
   bootstrap: [AppComponent]
 })
