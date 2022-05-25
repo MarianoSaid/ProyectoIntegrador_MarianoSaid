352ddcd9fad5d6787d960363eefc77c58a37acb9
MarianoSaid <marianoj_said@hotmail.com>
2022-05-25 16:08:24 -0300

    Conexxión con back end y front end

--- ProyectoIntegrador_MarianoSaid/backEnd/src/main/java/com/portfolio/mjs/Controller/PersonaController.java
+++ ProyectoIntegrador_MarianoSaid/backEnd/src/main/java/com/portfolio/mjs/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.mjs.Entity.Persona;
 import com.portfolio.mjs.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PersonaController {
+@CrossOrigin(origins = "http://localhost:4200")
+public class PersonaController { 
     @Autowired IPersonaService ipersonaService;
 
     @GetMapping("personas/traer")
@@ -47,4 +49,9 @@ public class PersonaController {
           ipersonaService.savePersona(persona);
           return persona;
         }
+    @GetMapping("/personas/traer/perfil")
+    public Persona findpersona(){
+        return ipersonaService.findPersona((long)1);
+    }
+   
 }
--- ProyectoIntegrador_MarianoSaid/backEnd/src/main/java/com/portfolio/mjs/Service/ImpPersonaService.java
+++ ProyectoIntegrador_MarianoSaid/backEnd/src/main/java/com/portfolio/mjs/Service/ImpPersonaService.java
@@ -33,4 +33,5 @@ import org.springframework.stereotype.Service;
        return persona;  
     }
     
+    
 } 
